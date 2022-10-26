d6a22e5c66efd98b1b416d33b8c7a5a314fe4329
anhkiet1227 <anhkiet1227@gmail.com>
2022-10-26 18:43:05 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-/* eslint-disable no-undef */
-/* eslint-disable react/jsx-no-undef */
-import "./App.css";
+/* import libraries */
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
-import Home from "./pages/common/Home/Home";
-import Post from "./pages/common/Post/Post";
-import Document from "./pages/common/DocumentList/DocumentList";
-import QandA from "./pages/common/QandA/QandA";
-import Upload from "./pages/common/Upload/Upload";
-import Search from "./pages/common/Search/Search";
-import Login from "./pages/common/Login/Login";
-import Register from "./pages/common/Register/Register";
+
+/* import my page web*/
+import Home from "./pages/Home/Home";
+import DocumentList from "./pages/DocumentList/DocumentList";
+import DocumentDetail from "./pages/DocumentDetail/DocumentDetail";
+import QandA from "./pages/QandA/QandA";
+import Upload from "./pages/Upload/Upload";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Search from "./pages/Search/Search";
 
 function App() {
   return (
-    <div style={{ minWidth: "320px", width: "100%", background: "white" }}>
-      <Router>
-        <Header />
-        <div id="header" style={{ height: "65px" }}>
-        </div>
-        <Routes>
-          this is the header
-          <Route exact path="/" element={Home} />
-          <Route exact path="/post" element={Post} />
-          <Route exact path="/document" element={DocumentList} />
-          <Route exact path="/document/:id" element={DocumentDetail} />
-          <Route exact path="/qanda" element={QandA} />
-          <Route exact path="/upload" element={Upload} />
-          <Route exact path="/login" element={Login} />
-          <Route exact path="/register" element={Register} />
-          <Route path="/search" exact element={Search} />
-        </Routes>
-        
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={Home} />
+        <Route exact path="/document" element={DocumentList} />
+        <Route exact path="/document/:id" element={DocumentDetail} />
+        <Route exact path="/qanda" element={QandA} />
+        <Route exact path="/upload" element={Upload} />
+        <Route exact path="/login" element={Login} />
+        <Route exact path="/register" element={Register} />
+        <Route path="/search" exact element={Search} />
+      </Routes>
+      <div className="App"></div>
+    </Router>
   );
 }
 
