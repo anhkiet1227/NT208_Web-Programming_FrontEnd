0941c64c72f40611658b4add49eeb8da64c920aa
anhkiet1227 <anhkiet1227@gmail.com>
2022-10-28 12:25:05 +0700

    .

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
 import '../../App.css';
 import { Button } from '../Button/Button';
--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from '../Button/Button';
+import { Button, ButtonRegister } from '../Button/Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
